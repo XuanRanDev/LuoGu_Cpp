8130453f1ccf58c8886960a1dc21dd9340abaa2a
渲染 <xuanrandev@qq.com>
2021-06-28 23:05:26 +0800

    add file

---
+++ qwtbz.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+
+string n,m;
+
+struct node{
+	int x;
+	string s;
+};
+
+
+void bfs(){
+	
+	node no;
+	no.x = ;
+	no.s = n;
+		
+}
+
+
+int main(){
+	
+	cin >> n >> m;
+	
+	
+	bfs();
+	
+	
+	
+	
+	
+	
+	return 0;
+} 
