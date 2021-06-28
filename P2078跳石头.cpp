c1f2127d8675598ae7a40deda85fa5dd876bc6dd
渲染 <xuanrandev@qq.com>
2021-06-28 23:04:18 +0800

    add file

---
+++ P2078跳石头.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+long B , N , M;
+int stone[500005];
+
+int main(){
+	
+	cin >> B >> N >> M;
+
+	
+	for(int i = 0 ; i < M ; i++){
+		cin >> stone[i]; 
+	}
+	
+	int L = 0, R = L;
+	
+	while(L <= R){
+		
+		int mid = (L+R) / 2;
+		if
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
